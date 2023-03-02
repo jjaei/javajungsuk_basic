8a7fd9facd8a6d0747a609c455b561c509548f45
USER <ieajnowk>
2023-03-02 16:32:26 +0900

    int/(float)int

---
+++ src/ch03/Ex03_07.java
@@ -0,0 +1,13 @@
+package ch03;
+
+public class Ex03_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(5/2);
+		System.out.println(5/(float)2);
+		
+	}
+
+}
