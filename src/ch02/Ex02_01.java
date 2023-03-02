95249a267b802cf8be8cfcecb8af72c9e9d174f0
USER <ieajnowk>
2023-03-02 12:02:13 +0900

    2+4=?

---
+++ src/ch02/Ex02_01.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class Ex02_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, World!");
+		System.out.print("Hello, ");  // 줄바꿈 없음.
+		System.out.println("World!");  // Hello 옆에 붙어서 출력됨.
+	}
+
+}
---
+++ src/ch02/Ex02_02.java
@@ -0,0 +1,14 @@
+package ch02;
+
+public class Ex02_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, World!");  
+		System.out.print("2+4=");
+		System.out.print(2+4);
+		
+	}
+
+}
