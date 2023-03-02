092b53ac8024bb9658bf76c89369eb42ce0f0394
USER <ieajnowk>
2023-03-02 20:48:28 +0900

    if문 활용

---
+++ src/ch04/Ex04_01.java
@@ -0,0 +1,15 @@
+package ch04;
+
+public class Ex04_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int score = 80;
+		
+		if(score>60) {
+			System.out.println("합격입니다.");
+		} // score가 60점 이상이면 합격입니다 출력
+	}
+
+}
