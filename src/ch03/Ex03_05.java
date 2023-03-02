40502d4e0bd99b2e81d7a5985eaf97605b743e1e
USER <ieajnowk>
2023-03-02 16:24:25 +0900

    int에 double 값을 대입하면?

---
+++ src/ch03/Ex03_05.java
@@ -0,0 +1,15 @@
+package ch03;
+
+public class Ex03_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double d = 78.9;
+		int score = (int)d;
+		System.out.println("score = " + score); // 78
+		System.out.println("d = " + d); // 78.9
+		
+	}
+
+}
