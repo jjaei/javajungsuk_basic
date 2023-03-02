7c9b17e3797cf2ffa9f2836bb2022e1b4a7de87e
USER <ieajnowk>
2023-03-02 16:11:12 +0900

    전위형, 후위형 알아보기

---
+++ src/ch03/Ex03_03.java
@@ -0,0 +1,16 @@
+package ch03;
+
+public class Ex03_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=5, j=5;
+		
+		System.out.println(i++); // 후위형
+		System.out.println(++j); // 전위형_증가하고 출력
+		System.out.println("i = " + i + ", j = " + j );
+		
+	}
+
+}
