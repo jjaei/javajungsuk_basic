d462bdfa192023591c0705d2d183dc95a24bd197
USER <ieajnowk>
2023-03-02 12:26:17 +0900

    사칙연산 출력하기

---
+++ src/ch02/Ex02_03.java
@@ -0,0 +1,14 @@
+package ch02;
+
+public class Ex02_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(3+2);  // 덧셈 출력하기, 5
+		System.out.println(3-2);  // 뺄셈 출력하기, 1
+		System.out.println(3*2);  // 곱셈 출력하기, 6
+		System.out.println(3/2);  // 나눗셈 출력하기, 1
+	}
+
+}
