6f51c9858d9648f8691e4aec46847a1a797d6f14
USER <ieajnowk>
2023-03-02 16:40:27 +0900

    byte

---
+++ src/ch03/Ex03_08.java
@@ -0,0 +1,14 @@
+package ch03;
+
+public class Ex03_08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte a = 10;
+		byte b = 30;
+		byte c = (byte)(a*b);
+		System.out.println(c); // 44
+	}
+
+}
