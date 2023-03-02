e0b0f90f5b9f0f8c68cee8ad5966d56479ea8c8a
USER <ieajnowk>
2023-03-02 16:55:56 +0900

    (-)가 붙었을 때의 나머지는?

---
+++ src/ch03/Ex03_13.java
@@ -0,0 +1,14 @@
+package ch03;
+
+public class Ex03_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(-10%7); // -3
+		System.out.println(10%-7);  // 3
+		System.out.println(-10%-7);  // 3
+		
+	}
+
+}
