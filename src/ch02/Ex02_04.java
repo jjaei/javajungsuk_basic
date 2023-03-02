3d4f426d4ca765708a19cb636dbd173c24c42e01
USER <ieajnowk>
2023-03-02 12:22:33 +0900

    변수 x에 값 대입하기

---
+++ src/ch02/Ex02_04.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class Ex02_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 5;
+		System.out.println(x);
+		
+		x = 10;
+		System.out.println(x);
+	}
+
+}
