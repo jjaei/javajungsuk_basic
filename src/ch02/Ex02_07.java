f23e5f926be04ed3e1cc04b8f08d434ecd23c3e3
USER <ieajnowk>
2023-03-02 13:26:24 +0900

    문자열과 숫자의 결합

---
+++ src/ch02/Ex02_07.java
@@ -0,0 +1,23 @@
+package ch02;
+
+public class Ex02_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "JA" + "VA";
+		String str = name + 7.9;
+		
+		System.out.println(name);  // JAVA
+		System.out.println(str);  // JAVA7.9
+		System.out.println(7 + " ");  // 7
+		System.out.println(7 + "");  // 7
+		System.out.println("" + 7);  // 7
+		System.out.println("" + "");  // 
+		System.out.println(7 + 7 + "");  // 14
+		System.out.println("" + 7 + 7);  // 77
+		
+		
+	}
+
+}
