15db561754064d5467679933a5091f785fb1e743
USER <ieajnowk>
2023-03-02 12:22:59 +0900

    문자, 문자열, 숫자 결합하기

---
+++ src/ch02/Ex02_12.java
@@ -0,0 +1,18 @@
+package ch02;
+
+public class Ex02_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "3";
+
+		System.out.println(str.charAt(0) - '0');
+		System.out.println('3' - '0' + 1);
+		System.out.println(Integer.parseInt(str) + 1);
+		System.out.println("3" + 1);
+		System.out.println(3 + '0');  // '0' -> 48
+		
+	}
+
+}
