1462ca34ef57fa608d659b6bafac881724ab8281
USER <ieajnowk>
2023-03-02 16:54:24 +0900

    몫과 나머지 구하기(/, %)

---
+++ src/ch03/Ex03_12.java
@@ -0,0 +1,16 @@
+package ch03;
+
+public class Ex03_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 10;
+		int y = 9;
+		
+		System.out.printf("%d을 %d로 나누면, %n", x, y);
+		System.out.printf("몫은 %d이고, 나머지는 %d입니다.",x/y,x%y);
+		//몫 1, 나머지 1
+	}
+
+}
