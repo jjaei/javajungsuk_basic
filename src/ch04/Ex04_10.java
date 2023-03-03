16b692d074ea55e4319b184dd40b6f2c2daca440
USER <ieajnowk>
2023-03-03 10:28:32 +0900

    for문 예제4_합계 구하기

---
+++ src/ch04/Ex04_10.java
@@ -0,0 +1,19 @@
+package ch04;
+
+public class Ex04_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum = 0;
+		
+		for(int i = 1; i <= 5 ; i++) {
+			sum += i;
+			System.out.printf("1부터 %d까지의 합: %d%n", i, sum);
+		}
+		
+		
+	}
+
+
+}
