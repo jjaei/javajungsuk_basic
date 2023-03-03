728205fb0e06f79647ccf1074a274f963e49077d
USER <ieajnowk>
2023-03-03 10:23:35 +0900

    for문 예제3

---
+++ src/ch04/Ex04_09.java
@@ -0,0 +1,17 @@
+package ch04;
+
+public class Ex04_09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0; i <5; i++) {
+			System.out.println(i); // i값을 반복해서 출력하기
+		}
+		
+		for(int i=0; i<5; i++) {
+			System.out.print(i);
+		}
+	}
+
+}
