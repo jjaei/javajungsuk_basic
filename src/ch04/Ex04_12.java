da883782ff372ab3b9a043af40e334c9d401745d
USER <ieajnowk>
2023-03-03 10:32:55 +0900

    while문 예제

---
+++ src/ch04/Ex04_12.java
@@ -0,0 +1,13 @@
+package ch04;
+
+public class Ex04_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 5;
+		while(i--!=0) { // i가 0이 될때까지 감소하며 반복하기
+			System.out.println(i);
+		}
+	}
+}
