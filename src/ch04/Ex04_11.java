2b184929f2f57abcf78637d4811b57778a8dc31c
USER <ieajnowk>
2023-03-03 10:30:34 +0900

    for문 예제5_별찍기(5*5)

---
+++ src/ch04/Ex04_11.java
@@ -0,0 +1,17 @@
+package ch04;
+
+public class Ex04_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
