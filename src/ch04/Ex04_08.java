5b7d971562393d53aa6f4720aa66e1e4f188779d
USER <ieajnowk>
2023-03-03 10:13:43 +0900

    for문 예제2

---
+++ src/ch04/Ex04_08.java
@@ -0,0 +1,13 @@
+package ch04;
+
+public class Ex04_08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1; i<=3; i++) { // 괄호 안에 문장(Hello)가 3번 출력된다.
+			System.out.println("Hello");
+		}
+	}
+
+}
